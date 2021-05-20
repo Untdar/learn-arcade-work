4397c020721c0d7a4b952709c928c47cd55cac02
Untdar <Untdar@gmail.com>
2021-05-20 20:09:31 +0300

    Start program window with 2 color rectangle

--- Lab 02 - Draw a Picture/drawing_samples.py
+++ Lab 02 - Draw a Picture/drawing_samples.py
@@ -1,12 +1,17 @@
 import arcade
 
 HEIGHT = 600
-WIDTH = 800
+WIDTH = 600
 TITLE_WINDOW = 'Test'
 
 
 def main():
     arcade.open_window(WIDTH, HEIGHT, TITLE_WINDOW)
+    arcade.set_background_color(arcade.csscolor.AQUA)
+    arcade.start_render()
+    arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+    arcade.finish_render()
+    arcade.run()
 
 
 if __name__ == "__main__":
